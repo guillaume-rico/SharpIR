ef4ed0181989bb8ad97521e01f0a2530229e9ed5
guillaume-rico <alliaume.rico@gmail.com>
2019-10-13 16:05:04 +0200

    Correction for 1080 model (Thanks to niek70)

--- SharpIR.cpp
+++ SharpIR.cpp
@@ -98,9 +98,9 @@ int SharpIR::distance() {
         
         // Different expressions required as the Photon has 12 bit ADCs vs 10 bit for Arduinos
         #ifdef ARDUINO
-          distanceCM = 27.728 * pow(map(ir_val[NB_SAMPLE / 2], 0, 1023, 0, 5000)/1000.0, -1.2045);
+          distanceCM = 29.988 * pow(map(ir_val[NB_SAMPLE / 2], 0, 1023, 0, 5000)/1000.0, -1.173);
         #elif defined(SPARK)
-          distanceCM = 27.728 * pow(map(ir_val[NB_SAMPLE / 2], 0, 4095, 0, 5000)/1000.0, -1.2045);
+          distanceCM = 29.988 * pow(map(ir_val[NB_SAMPLE / 2], 0, 4095, 0, 5000)/1000.0, -1.173);
         #endif
 
     } else if (_model==20150){
